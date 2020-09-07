a0629325990fd0954c7801fdd32b584357a5ac4e
thanh2107 <tyn01685732770@gmail.com>
2020-09-07 16:37:34 +0700

    add màng hình chào

--- app/src/main/java/com/example/gamebaucua/MainActivity.java
+++ app/src/main/java/com/example/gamebaucua/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.AnimationDrawable;
 import android.media.AudioManager;
